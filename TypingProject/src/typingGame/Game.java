cfcf1f3b9ffba111c8c56d2b652765841376b1a3
Student 6 <Student 6@10.8.33.94>
2017-05-04 13:13:14 -0400

    Import classes

---
+++ TypingProject/src/typingGame/Game.java
@@ -0,0 +1,23 @@
+package typingGame;
+
+import guiTeacher.GUIApplication;
+
+public class Game extends GUIApplication {
+
+	public Game(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
